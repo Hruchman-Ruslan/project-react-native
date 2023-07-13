7f626c42a60f3042bfdcd6fb638758e928a49345
Ruslan <Giovingo92@gmail.com>
2023-07-13 11:33:18 +0300

    fixed

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -2,22 +2,18 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export const Link = ({ title }) => {
   return (
-    <TouchableOpacity style={styles.wrapper}>
+    <TouchableOpacity>
       <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flexDirection: "row",
-    alignSelf: "center",
-    gap: 4,
-  },
   text: {
     fontFamily: "rb-regular",
     fontSize: 16,
     color: "#1B4371",
     fontWeight: "700",
+    textAlign: "center",
   },
 });
