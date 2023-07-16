f457c1c27a8f0d51ee99a5f9b1174d8c04112c7d
Ruslan <Giovingo92@gmail.com>
2023-07-16 14:25:24 +0300

    added form collection and keyboard auto-close to the login page

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Title } from "../components/Title";
 import { Input } from "../components/Input";
@@ -12,9 +12,15 @@ import { useState } from "react";
 
 export const LoginScreen = () => {
   const [isPasswordVisible, setPasswordVisible] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleTogglePassword = () => {
     setPasswordVisible(!isPasswordVisible);
+  };
+
+  const handleClickButton = () => {
+    Alert.alert("Credentials", `${email} + ${password}`);
   };
 
   return (
@@ -24,15 +30,20 @@ export const LoginScreen = () => {
           <Title title={"Sign in"} />
 
           <View style={{ width: "100%", marginBottom: 27, gap: 16 }}>
-            <Input defaultText={"Email"} />
-            <Input defaultText={"Password"} access={!isPasswordVisible} />
+            <Input defaultText={"Email"} text={email} setText={setEmail} />
+            <Input
+              defaultText={"Password"}
+              access={!isPasswordVisible}
+              text={password}
+              setText={setPassword}
+            />
             <ShowPassword
               isPasswordVisible={isPasswordVisible}
               onTogglePassword={handleTogglePassword}
             />
           </View>
 
-          <Button title={"Sign in"} />
+          <Button title={"Sign in"} handleClick={handleClickButton} />
           <Link title={"Don't have an account? Register"} />
         </SafeAreaView>
       </KeyboardWrapper>
