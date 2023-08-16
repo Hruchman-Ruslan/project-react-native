e8380ce1e863c71934973b28794f60a40714e42c
Ruslan <Giovingo92@gmail.com>
2023-08-16 19:14:11 +0300

    optimized authReducer

--- src/redux/auth/authReducer.js
+++ src/redux/auth/authReducer.js
@@ -1,12 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {
-  authSignUpUser,
-  authLogInUser,
-  authSignOutUser,
-  authOnStateChanged,
-} from "./authOperations";
 
-const state = {
+const initialState = {
   userID: null,
   login: null,
   email: null,
@@ -17,103 +11,39 @@ const state = {
 
 export const authSlice = createSlice({
   name: "auth",
-  initialState: state,
+  initialState: initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(authSignUpUser.pending, (state) => {
-        state.userID = null;
-        state.login = null;
-        state.email = null;
-        state.avatar = null;
-        state.isAuth = false;
-        state.error = false;
-      })
-      .addCase(authSignUpUser.fulfilled, (state, { payload }) => {
-        return {
-          ...state,
-          userID: payload.uid,
-          email: payload.email,
-          avatar: payload.photoURL,
-          login: payload.displayName,
-          isAuth: true,
-        };
-      })
-      .addCase(authSignUpUser.rejected, (state) => {
-        state.userID = null;
-        state.login = null;
-        state.email = null;
-        state.avatar = null;
-        state.isAuth = false;
-        state.error = true;
-      })
-      .addCase(authLogInUser.pending, (state) => {
-        state.userID = null;
-        state.login = null;
-        state.email = null;
-        state.avatar = null;
-        state.isAuth = false;
-        state.error = false;
-      })
-      .addCase(authLogInUser.fulfilled, (state, { payload }) => {
-        return {
-          ...state,
-          userID: payload.uid,
-          avatar: payload.photoURL,
-          email: payload.email,
-          login: payload.displayName,
-          isAuth: true,
-        };
-      })
-      .addCase(authLogInUser.rejected, (state) => {
-        state.userID = null;
-        state.login = null;
-        state.email = null;
-        state.avatar = null;
-        state.isAuth = false;
-        state.error = true;
-      })
-      .addCase(authSignOutUser.pending, (state) => {
-        state.userID = null;
-        state.login = null;
-        state.email = null;
-        state.avatar = null;
-        state.isAuth = false;
-        state.error = false;
-      })
-      .addCase(authSignOutUser.fulfilled, () => {
-        return state;
-      })
-      .addCase(authSignOutUser.rejected, (state) => {
-        state.userID = null;
-        state.login = null;
-        state.email = null;
-        state.avatar = null;
-        state.isAuth = false;
-        state.error = true;
-      })
-      .addCase(authOnStateChanged.pending, (state) => {
-        state.userID = null;
-        state.login = null;
-        state.email = null;
-        state.avatar = null;
-        state.isAuth = false;
-        state.error = false;
-      })
-      .addCase(authOnStateChanged.fulfilled, (state, { payload }) => {
-        return {
-          ...state,
-          ...payload,
-        };
-      })
-      .addCase(authOnStateChanged.rejected, (state) => {
-        state.userID = null;
-        state.login = null;
-        state.email = null;
-        state.avatar = null;
-        state.isAuth = false;
-        state.error = true;
-      });
+      .addMatcher(
+        (action) => action.type.endsWith("/pending"),
+        (state) => {
+          state.error = false;
+        }
+      )
+      .addMatcher(
+        (action) => action.type.endsWith("/fulfilled"),
+        (state, action) => {
+          const { uid, displayName, photoURL, email } = action.payload || {};
+          state.userID = uid || null;
+          state.login = displayName || null;
+          state.email = email || null;
+          state.avatar = photoURL || null;
+          state.isAuth = !!uid;
+          state.error = false;
+        }
+      )
+      .addMatcher(
+        (action) => action.type.endsWith("/rejected"),
+        (state) => {
+          state.userID = null;
+          state.login = null;
+          state.email = null;
+          state.avatar = null;
+          state.isAuth = false;
+          state.error = true;
+        }
+      );
   },
 });
 
