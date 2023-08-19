127021ebb3ae103aa569d86552f7219d2eacbef5
Ruslan <Giovingo92@gmail.com>
2023-08-19 23:30:17 +0300

    add formik and validation yup

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,9 +1,14 @@
-import { Alert, StyleSheet, View } from "react-native";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Title } from "../components/Title";
-import { Input } from "../components/Input";
-import { Button } from "../components/Button";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "../components/Link";
 import { Background } from "../components/Background";
@@ -12,6 +17,15 @@ import { ShowPassword } from "../components/ShowPassword";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { authLogInUser } from "../redux/auth/authOperations";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(6, "Password must be at least 6 characters long"),
+});
 
 const initialState = {
   email: "",
@@ -20,6 +34,7 @@ const initialState = {
 
 export default function LoginScreen() {
   const [state, setState] = useState(initialState);
+  const [isFocused, setIsFocused] = useState(false);
   const [isPasswordVisible, setPasswordVisible] = useState(false);
   const navigation = useNavigation();
 
@@ -29,66 +44,110 @@ export default function LoginScreen() {
     setPasswordVisible(!isPasswordVisible);
   };
 
-  const handleClickButton = async () => {
-    if (!state.email || !state.password) {
-      Alert.alert("Fill in all fields please!");
-      return;
-    }
-
-    try {
-      const resultAction = await dispatch(authLogInUser(state));
-
-      if (authLogInUser.rejected.match(resultAction)) {
-        Alert.alert("User not found. Please check your credentials.");
-        return;
-      }
-
-      navigation.navigate("BottomNavigator");
-    } catch (error) {
-      Alert.alert("An error occurred. Please try again later.");
-    }
+  const handleFocusInput = () => {
+    setIsFocused(true);
   };
+
+  const handleBlurInput = () => {
+    setIsFocused(false);
+  };
+
+  const inputStyle = isFocused
+    ? [styles.input, styles.inputFocus]
+    : styles.input;
 
   const handleClickOnText = () => {
     navigation.navigate("RegistrationScreen");
   };
 
   return (
-    <Background>
-      <KeyboardWrapper>
-        <SafeAreaView style={styles.wrapper}>
-          <Title title={"Sign in"} />
+    <Formik
+      initialValues={initialState}
+      validationSchema={validationSchema}
+      onSubmit={async (values, { setSubmitting }) => {
+        try {
+          const resultAction = await dispatch(authLogInUser(values));
 
-          <View style={styles.inputWrapper}>
-            <Input
-              placeholder={"Email"}
-              onChangeText={(value) =>
-                setState((prevState) => ({ ...prevState, email: value }))
-              }
-            />
-            <Input
-              placeholder={"Password"}
-              secureTextEntry={!isPasswordVisible}
-              autoCapitalize="none"
-              onChangeText={(value) =>
-                setState((prevState) => ({ ...prevState, password: value }))
-              }
-            />
-            <ShowPassword
-              isPasswordVisible={isPasswordVisible}
-              onTogglePassword={handleTogglePassword}
-            />
-          </View>
+          if (authLogInUser.rejected.match(resultAction)) {
+            Alert.alert("User not found. Please check your credentials.");
+          } else {
+            navigation.navigate("BottomNavigator");
+          }
+        } catch (error) {
+          Alert.alert("An error occurred. Please try again later.");
+        }
+        setSubmitting(false);
+      }}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        touched,
+        isSubmitting,
+      }) => (
+        <Background>
+          <KeyboardWrapper>
+            <SafeAreaView style={styles.wrapper}>
+              <Title title={"Sign in"} />
 
-          <Button title={"Sign in"} handleClick={handleClickButton} />
-          <Link
-            title={"Don't have an account? Register"}
-            handleClickOnText={handleClickOnText}
-          />
-        </SafeAreaView>
-      </KeyboardWrapper>
-      <StatusBar style="auto" />
-    </Background>
+              <View style={styles.inputWrapper}>
+                {touched.email && errors.email && (
+                  <Text style={styles.errorTextEmail}>{errors.email}</Text>
+                )}
+                <TextInput
+                  style={inputStyle}
+                  onFocus={handleFocusInput}
+                  onBlur={(handleBlur("email"), handleBlurInput)}
+                  placeholder={"Email"}
+                  onChangeText={handleChange("email")}
+                  value={values.email}
+                  name="email"
+                />
+
+                {touched.password && errors.password && (
+                  <Text style={styles.errorTextPassword}>
+                    {errors.password}
+                  </Text>
+                )}
+                <TextInput
+                  style={inputStyle}
+                  onFocus={handleFocusInput}
+                  onBlur={(handleBlur("password"), handleBlurInput)}
+                  placeholder={"Password"}
+                  secureTextEntry={!isPasswordVisible}
+                  autoCapitalize="none"
+                  onChangeText={handleChange("password")}
+                  value={values.password}
+                  name="password"
+                />
+
+                <ShowPassword
+                  isPasswordVisible={isPasswordVisible}
+                  onTogglePassword={handleTogglePassword}
+                />
+              </View>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleSubmit}
+                disabled={isSubmitting}
+              >
+                <Text style={styles.buttonText}>Sign In</Text>
+              </TouchableOpacity>
+
+              <Link
+                title={"Don't have an account? Register"}
+                handleClickOnText={handleClickOnText}
+              />
+            </SafeAreaView>
+          </KeyboardWrapper>
+          <StatusBar style="auto" />
+        </Background>
+      )}
+    </Formik>
   );
 }
 
@@ -103,8 +162,67 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   inputWrapper: {
+    marginTop: 10,
     width: "100%",
     marginBottom: 27,
     gap: 16,
+  },
+  input: {
+    position: "relative",
+    height: 50,
+    borderRadius: 8,
+    paddingLeft: 16,
+    paddingHorizontal: 16,
+    color: "#212121",
+    fontFamily: "rb-regular",
+    fontSize: 16,
+    lineHeight: 16,
+    backgroundColor: "#F6F6F6",
+    fontWeight: "700",
+  },
+  inputFocus: {
+    backgroundColor: "transparent",
+    borderWidth: 1,
+    borderColor: "#FF6C00",
+  },
+  button: {
+    width: "100%",
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    marginBottom: 16,
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+  },
+  buttonText: {
+    fontFamily: "rb-regular",
+    fontSize: 16,
+    fontWeight: "700",
+    textAlign: "center",
+    color: "#FFF",
+  },
+  errorTextEmail: {
+    position: "absolute",
+    color: "#FF6C00",
+    marginTop: 5,
+    fontSize: 16,
+    fontFamily: "rb-regular",
+    textAlign: "center",
+    fontWeight: "700",
+    top: -35,
+    bottom: -20,
+    left: 0,
+    right: 0,
+  },
+  errorTextPassword: {
+    position: "absolute",
+    color: "#FF6C00",
+    marginTop: 5,
+    fontSize: 16,
+    fontFamily: "rb-regular",
+    textAlign: "center",
+    fontWeight: "700",
+    bottom: -15,
+    left: 0,
+    right: 0,
   },
 });
--- src/screens/RegistrationScreen.jsx
+++ src/screens/RegistrationScreen.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { StyleSheet, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Title } from "../components/Title";
-import { Input } from "../components/Input";
-import { Button } from "../components/Button";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "../components/Link";
 import { AddPhoto } from "../components/AddPhoto";
@@ -15,6 +19,16 @@ import { useDispatch } from "react-redux";
 import { authSignUpUser } from "../redux/auth/authOperations";
 import * as ImagePicker from "expo-image-picker";
 import { Alert } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  login: Yup.string().required("Login is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(6, "Password must be at least 6 characters long"),
+});
 
 const initialState = {
   login: "",
@@ -25,6 +39,7 @@ const initialState = {
 export default function RegistrationScreen() {
   const [state, setState] = useState(initialState);
   const [isPasswordVisible, setPasswordVisible] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
   const [avatar, setAvatar] = useState("");
   const navigation = useNavigation();
 
@@ -64,68 +79,128 @@ export default function RegistrationScreen() {
       Alert.alert("Error", `${error.message}`);
     }
   };
+  //   if (!state.email || !state.password || !state.login) {
+  //     Alert.alert("Fill in all fields please!");
+  //     return;
+  //   }
 
-  const handleClickButton = () => {
-    if (!state.email || !state.password || !state.login) {
-      Alert.alert("Fill in all fields please!");
-      return;
-    }
-
-    dispatch(authSignUpUser({ ...state, avatar }));
-    setState(initialState);
-    navigation.navigate("BottomNavigator");
-  };
+  //   dispatch(authSignUpUser({ ...state, avatar }));
+  //   setState(initialState);
+  //   navigation.navigate("BottomNavigator");
+  // };
 
   const handleClickOnText = () => {
     navigation.navigate("LoginScreen");
   };
 
+  const inputStyle = isFocused
+    ? [styles.input, styles.inputFocus]
+    : styles.input;
+
+  const handleFocusInput = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlurInput = () => {
+    setIsFocused(false);
+  };
+
   return (
-    <Background>
-      <KeyboardWrapper>
-        <SafeAreaView style={styles.wrapper}>
-          <AddPhoto avatar={avatar} onPress={chooseImage} />
-          <Title title={"Registration"} />
+    <Formik
+      initialValues={initialState}
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        try {
+          dispatch(authSignUpUser({ ...values, avatar }));
+          setState(initialState);
+          navigation.navigate("BottomNavigator");
+        } catch (error) {
+          Alert.alert("An error occurred. Please try again later.");
+        }
+        setSubmitting(false);
+      }}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        touched,
+        isSubmitting,
+      }) => (
+        <Background>
+          <KeyboardWrapper>
+            <SafeAreaView style={styles.wrapper}>
+              <AddPhoto avatar={avatar} onPress={chooseImage} />
+              <Title title={"Registration"} />
 
-          <View style={styles.inputWrapper}>
-            <Input
-              placeholder={"Login"}
-              onChangeText={(value) =>
-                setState((prevState) => ({ ...prevState, login: value }))
-              }
-            />
-            <Input
-              placeholder={"Email"}
-              onChangeText={(value) =>
-                setState((prevState) => ({ ...prevState, email: value }))
-              }
-            />
-            <Input
-              placeholder={"Password"}
-              secureTextEntry={!isPasswordVisible}
-              autoCapitalize="none"
-              onChangeText={(value) =>
-                setState((prevState) => ({
-                  ...prevState,
-                  password: value,
-                }))
-              }
-            />
-            <ShowPassword
-              isPasswordVisible={isPasswordVisible}
-              onTogglePassword={handleTogglePassword}
-            />
-          </View>
+              <View style={styles.inputWrapper}>
+                {touched.login && errors.login && (
+                  <Text style={styles.errorTextLogin}>{errors.login}</Text>
+                )}
+                <TextInput
+                  style={inputStyle}
+                  onFocus={handleFocusInput}
+                  placeholder={"Login"}
+                  onChangeText={handleChange("login")}
+                  onBlur={(handleBlur("login"), handleBlurInput)}
+                  value={values.login}
+                  name="login"
+                />
 
-          <Button title={"Register"} handleClick={handleClickButton} />
-          <Link
-            title={"Already have an account? Sign In"}
-            handleClickOnText={handleClickOnText}
-          />
-        </SafeAreaView>
-      </KeyboardWrapper>
-      <StatusBar style="auto" />
-    </Background>
+                {touched.email && errors.email && (
+                  <Text style={styles.errorTextEmail}>{errors.email}</Text>
+                )}
+                <TextInput
+                  style={inputStyle}
+                  onFocus={handleFocusInput}
+                  onBlur={(handleBlur("email"), handleBlurInput)}
+                  placeholder={"Email"}
+                  onChangeText={handleChange("email")}
+                  value={values.email}
+                  name="email"
+                />
+
+                {touched.password && errors.password && (
+                  <Text style={styles.errorTextPassword}>
+                    {errors.password}
+                  </Text>
+                )}
+                <TextInput
+                  style={inputStyle}
+                  onFocus={handleFocusInput}
+                  onBlur={(handleBlur("password"), handleBlurInput)}
+                  placeholder={"Password"}
+                  secureTextEntry={!isPasswordVisible}
+                  autoCapitalize="none"
+                  onChangeText={handleChange("password")}
+                  value={values.password}
+                  name="password"
+                />
+                <ShowPassword
+                  isPasswordVisible={isPasswordVisible}
+                  onTogglePassword={handleTogglePassword}
+                />
+              </View>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleSubmit}
+                disabled={isSubmitting}
+              >
+                <Text style={styles.buttonText}>Sign up</Text>
+              </TouchableOpacity>
+              <Link
+                title={"Already have an account? Sign In"}
+                handleClickOnText={handleClickOnText}
+              />
+            </SafeAreaView>
+          </KeyboardWrapper>
+          <StatusBar style="auto" />
+        </Background>
+      )}
+    </Formik>
   );
 }
 
@@ -140,8 +215,77 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   inputWrapper: {
+    marginTop: 10,
     width: "100%",
     marginBottom: 27,
     gap: 16,
+  },
+  button: {
+    width: "100%",
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    marginBottom: 16,
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+  },
+  buttonText: {
+    fontFamily: "rb-regular",
+    fontSize: 16,
+    fontWeight: "700",
+    textAlign: "center",
+    color: "#FFF",
+  },
+  input: {
+    height: 50,
+    borderRadius: 8,
+    paddingLeft: 16,
+    paddingHorizontal: 16,
+    color: "#212121",
+    fontFamily: "rb-regular",
+    fontSize: 16,
+    lineHeight: 16,
+    backgroundColor: "#F6F6F6",
+    fontWeight: "700",
+  },
+  inputFocus: {
+    backgroundColor: "transparent",
+    borderWidth: 1,
+    borderColor: "#FF6C00",
+  },
+  errorTextEmail: {
+    position: "absolute",
+    color: "#FF6C00",
+    marginTop: 5,
+    fontSize: 16,
+    fontFamily: "rb-regular",
+    textAlign: "center",
+    fontWeight: "700",
+    top: 75,
+    left: 210,
+    right: 0,
+  },
+  errorTextPassword: {
+    position: "absolute",
+    color: "#FF6C00",
+    marginTop: 5,
+    fontSize: 16,
+    fontFamily: "rb-regular",
+    textAlign: "center",
+    fontWeight: "700",
+    bottom: -15,
+    left: 0,
+    right: 0,
+  },
+  errorTextLogin: {
+    position: "absolute",
+    color: "#FF6C00",
+    marginTop: 5,
+    fontSize: 16,
+    fontFamily: "rb-regular",
+    textAlign: "center",
+    fontWeight: "700",
+    top: -35,
+    left: 0,
+    right: 0,
   },
 });
